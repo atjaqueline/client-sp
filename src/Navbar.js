41fc9b97c0d5de779d08ec7cc7506ae4fda9e928
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-05-13 19:30:42 -0500

    add api

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="/">Home </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#"></a>
--- src/messagesList.js
+++ src/messagesList.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import NewMessageForm from "./newMessageForm";
-
+import "./messagesList.css"
 
 
 const MessagesList = () => {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+
+
+    async function getJSONData() {
+      const response = await fetch("http://localhost:8080/api/messages");
+      const jsonData = await response.json();
+      setMessages(jsonData);
+      console.log(jsonData);
+    }
+getJSONData();
+}, []);
+
+
+
   return (
     <div>
-      <p>All messages here</p>
+      <p>Messages</p>
     <ul>
-      <div>
-        <li>id 1 message
-          <a href="/messages/${id}">see more</a> 
-          </li>
-        
-        </div>
+
+      {messages.map(message => 
+         <div>
+           
+         <li className="msg-container"> 
+         <img src=""></img>
+         <p>Title</p>
+         <p>{message.messages}</p>
+           <a href="/messages/${id}">see more</a> 
+           </li>
+         
+         </div>
+        )}
+     
     </ul>
     <NewMessageForm/>
     </div>
