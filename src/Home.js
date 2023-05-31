0577a0759f86a6789662946a65d30abef617470b
Jaqueline Coelho <88630180+jaquelinelin@users.noreply.github.com>
2023-05-30 20:31:04 -0400

    add home page

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-
-
   return (
- <div>
-   <h1>Home</h1>
- </div>
-  )
-}
+    <div>
+      <div className="update-container">
+        <div className="header-container"><p>SODEX Updates</p>
+        </div>
+        <div className="img-container">
+          <img></img>
+          <a>PREVIOUS WEEK</a>
+        </div>
+
+        <div className="button-container">
+          <p>Are you ready?</p>
+          <button>Let's go!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
